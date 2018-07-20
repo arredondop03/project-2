0eee70a76b98b6f728bd775e282c58be99e2bbe2
Paola Arredondo <arredondop03@gmail.com>
2018-07-20 09:39:36 -0400

    landing page, login and signup style ready

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,3 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+$('.datetimepicker').datetimepicker({
+  icons: {
+      time: "fa fa-clock-o",
+      date: "fa fa-calendar",
+      up: "fa fa-chevron-up",
+      down: "fa fa-chevron-down",
+      previous: 'fa fa-chevron-left',
+      next: 'fa fa-chevron-right',
+      today: 'fa fa-screenshot',
+      clear: 'fa fa-trash',
+      close: 'fa fa-remove'
+  }
+});
